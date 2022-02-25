2667f279e03818f38cdbc3daa753e188deb82a78
unknown <tucker.serwadda@xfact.com>
2022-02-25 14:28:13 -0500

    first done patient register commit

---
+++ src/main/java/com/interview/donepatientregister/DonePatientRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.interview.donepatientregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonePatientRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonePatientRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/donepatientregister/DonePatientRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.donepatientregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonePatientRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
